7ba3ad19d6985cf4e5a812d0c6ab3d67bfb39178
leandrop <leandro.p.n96@gmail.com>
2022-01-03 14:15:33 -0300

    feat: :sparkles: Foi adicionado faunaDB e pagamento com stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,17 +1,38 @@
+import { useSession, signIn } from 'next-auth/react'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 import style from './style.module.scss'
 
 interface SubscribeButtonProps {
     priceId: String
 }
 
-export function SubscribeButton({priceId}: SubscribeButtonProps){
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const { status } = useSession()
+    async function handlreSubscribe() {
+        if (status !== 'authenticated') {
+            signIn('github')
+            return
+        }
+        try {
+            const response = await api.post('subscribe')
+
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({ sessionId: sessionId })
+        } catch (err) {
+            alert(err.message);
+        }
+    }
     return (
         <button
-        type="button"
-        className={style.subscribeButton}
-
+            type="button"
+            className={style.subscribeButton}
+            onClick={handlreSubscribe}
         >
             Subscribe
         </button>
     )
-    }
+}
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query } from 'faunadb'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
 
@@ -11,9 +13,42 @@ export default NextAuth({
                 params: {
                     scope: 'read:user',
                 }
-            }
+            },
         }),
 
     ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            const { email } = user
+
+            try {
+                await fauna.query(
+                    query.If(
+                        query.Not(
+                            query.Exists(
+                                query.Match(
+                                    query.Index('user_by_email'),
+                                    query.Casefold(email)
+                                )
+                            )
+                        ),
+                        query.Create(query.Collection('users'),
+                            { data: { email: email } }
+                        ),
+                        query.Get(
+                            query.Match(
+                                query.Index('user_by_email'),
+                                query.Casefold(email)
+                            )
+                        )
+                    )
+                )
+                return true
+            } catch (err) {
+                console.log(err)
+                return false
+            }
+        }
+    }
 
 })
